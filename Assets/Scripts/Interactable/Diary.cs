e299ab6dc4b9999b7590361b3e80aab56f44b947
johnlim612 <47646455+johnlim612@users.noreply.github.com>
2022-02-14 18:46:22 -0800

    item dialogue system implelmented

--- Assets/Scripts/Interactable/Diary.cs
+++ Assets/Scripts/Interactable/Diary.cs
@@ -1,6 +1,9 @@
 public class Diary : InteractableObject {
     private string _diaryUnusable = "You need to repair the problem on the spaceship before you can use the diary";
     private string _diaryUsed = "You wrote on the diary of today's events";
+    public string DiaryUsed {
+        get { return _diaryUsed; }
+	}
 
     public override void InteractObject() {
         if (GameManager.SabotageId != null) {
@@ -11,7 +14,7 @@ public class Diary : InteractableObject {
     }
 
     private void OpenDiary() {
-        print(_diaryUsed);
+        //FindObjectOfType<UI.UIDialogueManager>().StartDiaryDialogue(this);
         EndDay();
     }
 

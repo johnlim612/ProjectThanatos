4140e7077049a9f8d3075940d1a5eb8a71aba2de
kiwon132445 <kiwon132445@gmail.com>
2022-02-14 18:18:03 -0800

    something

--- Assets/Scripts/Interactable/InteractableObject.cs
+++ Assets/Scripts/Interactable/InteractableObject.cs
@@ -4,6 +4,11 @@ using UnityEngine.InputSystem;
 public class InteractableObject : MonoBehaviour {
     private string _interactableText = "Press Space";
     private bool _interactableCollided = false;
+    private BoxCollider2D _boxCol2D;
+
+    private void Awake() {
+        _boxCol2D = GetComponent<BoxCollider2D>();
+    }
 
     public void OnInteract(InputValue value) {
         if (_interactableCollided) {
@@ -30,7 +35,7 @@ public class InteractableObject : MonoBehaviour {
         if (_interactableCollided) {
             var position = Camera.main.WorldToScreenPoint(gameObject.transform.position);
             var textSize = GUI.skin.box.CalcSize(new GUIContent(_interactableText));
-            GUI.Box(new Rect(position.x - textSize.x/2, Screen.height - position.y + textSize.y/2, textSize.x, textSize.y), _interactableText);
+            GUI.Box(new Rect(position.x - textSize.x/2, Screen.height - position.y + _boxCol2D.size.y/2, textSize.x, textSize.y), _interactableText);
         }
     }
 }

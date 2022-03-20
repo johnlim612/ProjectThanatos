1e397dfc647ad961e4bac8d92879d2ee8ce83667
Charles <charleshuang97@gmail.com>
2022-03-20 06:08:34 -0700

    merge conflicts

--- Assets/Scripts/Tablet/TabletManager.cs
+++ Assets/Scripts/Tablet/TabletManager.cs
@@ -1,14 +1,12 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TabletManager : MonoBehaviour {
     private string _questLog;
     private string _diaryEntry;
 
-    private void Awake() {
+    private void Start() {
         _questLog = "";
         _diaryEntry = "";
-
         DialogueDataManager.Instance.Initialize(UI.EntityType.Diary,
             Constants.TabletKey, 1); // change to current day instead
     }

51ef40d17b9730bad2ced723574c7dd5879b8214
acm <alexis_cm@live.com>
2022-03-21 19:13:02 -0700

    Update Constants file

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -4,19 +4,25 @@
 public static class Constants {
     public const string PlayerKey = "Player";
     public const string SabotageDialogueKey = "sabotage";
-    public const string RandomEventDialogueKey = "sidequest";
+    public const string RandomEventDialogueKey = "Side-Quest"; // needed?
     public const string CharacterDialogueKey = "backstory";
-    public const string GreetingKey = "greeting";
-    public const string DialoguePromptKey = "prompt";
-    public const string DialogueNPCSentenceKey = "sentence";
-    public const string SystemAlertKey = "alert";
-    public const string SystemMessageKey = "message";
+    public const string GreetingKey = "Greeting";
+    public const string DialoguePromptKey = "prompt"; // needed?
+    public const string DialogueNPCSentenceKey = "sentence"; // needed?
+    public const string SystemAlertKey = "Alert";
+    public const string SystemMessageKey = "message"; // needed?
     public const string SystemAnnouncement = "SystemAnnouncement";
     public const string DiaryKey = "diary";
-    public const string QuestLogKey = "quest";
+    public const string QuestLogKey = "Quest";
     public const string TabletKey = "Tablet"; // Name of the Tablet.json file
     public const string NpcKey = "NPC";
     public const float MapXRatio = 4.796747967479674796747967479674796747967479674796694208008135f;
     public const float MapYRatio = 3.615384615384615384615384615384615384615384615384694208008135f;
     public const int QuestPrompt = 2;
+    public const string JohnnyWalker = "johnny-walker";
+    public const string WonKi = "won-ki";
+    public const string RachelLumina = "rachel-lumina";
+    public const string YuriMiko = "yuri-miko";
+    public const string SideQuest = "Side-Quest";
+    public const string SideQuestComplete = "Side-Quest-Complete";
 }

05b1c1228f437cc3c59584ab650cb9e052844b1c
Charles <charleshuang97@gmail.com>
2022-03-22 01:37:31 -0700

    update

--- Assets/Scripts/Flashlight.cs
+++
@@ -1,7 +0,0 @@
-public class Flashlight : Item {
-    public override void InteractObject() {
-        base.InteractObject();
-        Obtained = true;
-        print("Flashlight obtained: " + Obtained);
-    }
-}

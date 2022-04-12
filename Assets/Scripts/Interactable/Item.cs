cf6af249c6d0dc44a4a121585c29222e2b71d9f4
kiwon132445 <kiwon132445@gmail.com>
2022-04-11 19:19:51 -0700

    outline working

--- Assets/Scripts/Interactable/Item.cs
+++ Assets/Scripts/Interactable/Item.cs
@@ -4,19 +4,13 @@ using UnityEngine;
 public class Item : InteractableObject {
     
     [SerializeField] private string[] _descriptions;
-    [SerializeField] private GameObject _player;
-    [SerializeField] private float _glowRange = 7;
     private Queue<(string, string)> _descriptionQueue = new Queue<(string, string)>();
-    private cakeslice.Outline _outline;
-
 
     [HideInInspector]
     public bool Obtained { get; set; }
 
 	void Awake() {
         Obtained = false;
-        _player = GameObject.Find(Constants.PlayerKey);
-        _outline = gameObject.GetComponent<cakeslice.Outline>();
     }
 
 	void Start() {
@@ -24,6 +18,7 @@ public class Item : InteractableObject {
 	}
 
 	public override void InteractObject() {
+        print("Item was Detected!!!!!");
         Interact();
     }
 
@@ -31,19 +26,6 @@ public class Item : InteractableObject {
         get { return _descriptionQueue; }
     }
 
-	void Update() {
-        if (_outline != null) {
-            RegulateGlow();
-		}
-	}
-	public void RegulateGlow() {
-        float dis = Vector3.Distance(transform.position, _player.transform.position);
-        if (dis < _glowRange) {
-            _outline.enabled = true; 
-        } else {
-            _outline.enabled = false;
-		}
-    }
 
     public void AddToQueue(string[] sentences) {
         foreach (string sentence in sentences) {
@@ -53,7 +35,6 @@ public class Item : InteractableObject {
 
     private void Interact() {
         AddToQueue(_descriptions);
-        print("Item was Detected!!!!!");
         FindObjectOfType<UI.UIDialogueManager>().InitializeDialogue(UI.EntityType.Item, this);
         Obtained = true;
         gameObject.SetActive(false);
